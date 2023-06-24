f972cba06825685e6c08425cfca76900fe033cb3
HaykaramNik <87484645+HaykaramNik@users.noreply.github.com>
2023-06-24 11:32:52 +0400

    forms

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -1,22 +1,37 @@
 import { useState, useEffect } from 'react';
 import Post from './Post';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch(API_URL)
       .then((responce) => responce.json())
       .then((json) => setPosts(json))
-      .catch((error) => console.log(error.massage));
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
   }, []);
 
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
-    <div>
-      {posts.map((post) => {
-        return <Post key={post.id} {...post} />;
-      })}
-    </div>
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => {
+          return <Post key={post.id} {...post} />;
+        })
+      )}
+    </>
   );
 }
 
